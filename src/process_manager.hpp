a819f7abe9c09c18f7b8ae93a276dcee400295cc
DeNiCoN <denicon1234@gmail.com>
2020-09-16 20:04:56 +0300

    ProcessManager interface

---
+++ src/process_manager.hpp
@@ -0,0 +1,30 @@
+#pragma once
+#include <memory>
+
+namespace Transport
+{
+    class Process
+    {
+    public:
+        enum class State
+        {
+            UNINITIALIZED,
+            RUNNING,
+            ENDED
+        };
+
+        virtual void init() {}
+        virtual void update(double delta) = 0;
+        virtual ~Process() = default;
+        void setNext(std::unique_ptr<Process>);
+        const Process& getNext() const;
+        std::unique_ptr<Process> takeNext();
+    protected:
+        State state = State::UNINITIALIZED;
+    };
+
+    class ProcessManager
+    {
+
+    };
+}
