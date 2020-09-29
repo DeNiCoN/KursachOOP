fd6323019d5376197b0209f2325690e2afb6557d
DeNiCoN <denicon1234@gmail.com>
2020-09-29 23:54:24 +0300

    Graphics addElement implementation

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -2,8 +2,46 @@
 
 namespace Transport
 {
+    using namespace std;
+
     Graphics::Graphics(Renderer& renderer)
         : m_renderer(renderer)
     {
+    }
+
+
+    void Graphics::addVertex(std::string name, VertexPtr ptr)
+    {
+        GraphicsVertex n {move(name), move(ptr)};
+        m_vertexes.insert({n.name, move(n)});
+    }
+    void Graphics::addVechicle(std::string name, VechiclePtr ptr)
+    {
+        GraphicsVechicle n {move(name), move(ptr)};
+        m_vechicles.insert({n.name, move(n)});
+    }
+
+    void Graphics::addRoad(const std::string& from, const std::string& to,
+                           RoadPtr ptr)
+    {
+        m_roads.push_back(GraphicsRoad{m_vertexes.at(from), m_vertexes.at(to), move(ptr)});
+        m_graph.at(from).at(to) = m_roads.back();
+        m_graph.at(to).at(from) = m_roads.back();
+    }
+
+    void Graphics::vechicleRideRoad(const std::string& vechicleName,
+                                    const std::string& from, const std::string& to)
+    {
+
+    }
+    void Graphics::vechicleRideVertex(const std::string& vechicleName,
+                                      const std::string& vertexName)
+    {
+
+    }
+
+    void Graphics::update(double delta)
+    {
+        //m_pManager.update(delta);
     }
 }
--- src/graphics.hpp
+++ src/graphics.hpp
@@ -8,6 +8,27 @@
 
 namespace Transport
 {
+    struct GraphicsVertex
+    {
+        const std::string name;
+        VertexPtr vertexPtr;
+        glm::vec2 position;
+    };
+
+    struct GraphicsRoad
+    {
+        GraphicsVertex& from;
+        GraphicsVertex& to;
+        RoadPtr roadPtr;
+    };
+
+    struct GraphicsVechicle
+    {
+        const std::string name;
+        VechiclePtr vechiclePtr;
+        glm::vec2 position;
+    };
+
     /*
     ** Отвечает за рисование вершин, дорог и машин, их позицию
     */
@@ -16,25 +37,33 @@ namespace Transport
     private:
         using Renderer = EmptyRenderer;
     public:
-        struct Vertex
-        {
-            glm::vec2 position;
-        };
-
-        struct Road
-        {
-            Vertex from, to;
-        };
 
         Graphics(Renderer& renderer);
 
-        void addVertex(std::string name, ::Transport::VertexPtr);
-        void addVechicle(std::string name, ::Transport::VechiclePtr);
-        void addRoad(std::string from, std::string to, ::Transport::RoadPtr);
+        void addVertex(std::string name, VertexPtr);
+        void addVechicle(std::string name, VechiclePtr);
+        void addRoad(const std::string& from, const std::string& to, RoadPtr);
+
+        void vechicleRideRoad(const std::string& vechicleName,
+                              const std::string& from, const std::string& to);
+        void vechicleRideVertex(const std::string& vechicleName,
+                                const std::string& vertexName);
 
         void update(double delta);
     private:
         ProcessManager m_pManager;
         Renderer& m_renderer;
+
+        //Using string_view for memory efficiency at cost of
+        //some cpu time while filling up graph
+        std::unordered_map<std::string_view, GraphicsVertex> m_vertexes;
+        std::vector<GraphicsRoad> m_roads;
+        std::unordered_map<std::string_view, GraphicsVechicle> m_vechicles;
+
+        std::unordered_map<std::string_view,
+                           std::unordered_map<std::string_view,
+                                              std::reference_wrapper<GraphicsRoad>
+                                              >> m_graph;
+       
     };
 }
