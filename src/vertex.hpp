3df2e4dc0eced69d234e2e009684d393fc35e8e7
DeNiCoN <denicon1234@gmail.com>
2020-11-22 19:31:23 +0200

    Moved vertex_base to a different file

--- src/vertex.hpp
+++ src/vertex.hpp
@@ -29,21 +29,4 @@ namespace transport
 
     using VertexPtr = std::unique_ptr<Vertex>;
 
-    template<class T>
-    struct VertexBaseSingle : public virtual Vertex
-    {
-        using Vertex::Visit;
-        double Visit(T& t) override { return VisitDefault(t); }
-    };
-
-    template<class... T>
-    struct VertexBaseMultiple : VertexBaseSingle<T>...
-    {
-        using VertexBaseSingle<T>::Visit...;
-    };
-
-    struct VertexBase : VertexBaseMultiple<
-        TestVehicle, SecondTestVehicle
-        >
-    {};
 }
