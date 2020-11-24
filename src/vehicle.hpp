5d6eb3954e20cf59fe543e15a501bad75c8b86e8
DeNiCoN <denicon1234@gmail.com>
2020-11-24 20:00:13 +0200

    Vertex-vehicle vistor now returns process

--- src/vehicle.hpp
+++ src/vehicle.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include "vertex.hpp"
+#include <string>
 #include <nlohmann/json.hpp>
 #include <memory>
 #include <iostream>
+#include "process_manager.hpp"
 #include "json_deserializable.hpp"
 #include "graphics_object.hpp"
 
@@ -11,7 +13,7 @@ namespace transport
     class Vehicle : public JsonDeserializable, public GraphicsObject
     {
     public:
-        virtual double Accept(Vertex&) = 0;
+        virtual ProcessPtr Accept(Vertex&) = 0;
         virtual double GetSpeed() { return 1.; }
         const std::string& GetName() const { return name_; }
         void SetName(const std::string& name) { name_ = name; }
@@ -25,7 +27,7 @@ namespace transport
     class VehicleBase : public Vehicle
     {
     public:
-        double Accept(Vertex& vert) override
+        ProcessPtr Accept(Vertex& vert) override
         {
             return vert.Visit(*static_cast<Veh*>(this));
         }
