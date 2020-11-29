1f9c46a289753e72bb0373fce3a825dc48af73d1
Max <makspr25@gmail.com>
2020-11-29 19:09:34 +0200

    Added ChangeVertexType/little fixes

--- src/graph.hpp
+++ src/graph.hpp
@@ -13,20 +13,22 @@ namespace transport
 	class Graph
 	{
 	private:
-		int vertices_quan_;
-		int fullness_ = 0, vertex_type_quan_;
+		int vertices_quan_, vertex_type_quan_;
+		int fullness_ = 0;
+		std::vector<std::vector<int>> type_id_lists_;
+		std::vector<std::vector<int>> callback_id_;
+		std::vector<int> deleted_type_;
+
 	public:
 		std::vector<std::vector<int>> adjecent_vert;
 		std::vector<std::vector<double>> edge_weight_vec;		// Every road is stored twice
-		std::vector<std::vector<int>> type_id_lists;
-		std::vector<std::vector<int>> callback_id;
-		
+
 		int AddVertex(const std::vector<VertexType> type);
 		void AddEdge(const int f_vertex, const int s_vertex, const double edge_weight);
 		int GetRandVertex() const;
 		int GetRandVertex(const VertexType type) const;
-		void ChangeVertexType(const VertexType type, const int id);
-		int CountVertexType();
+		void ChangeVertexType(const std::vector<VertexType> new_type, const int id);
+		int CountVertexType() const;
 
 		Graph(int const vert_quan);
 //		~Graph() = default;
