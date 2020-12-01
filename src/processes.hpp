6bcbc96ef054cb905122cf6372d03707a9c8725b
DeNiCoN <denicon1234@gmail.com>
2020-12-01 17:51:34 +0200

    Some new Processes

--- src/processes.hpp
+++ src/processes.hpp
@@ -28,5 +28,66 @@ namespace transport
         public:
             void Update(double delta) {}
         };
+
+        template<typename C>
+        class CallBack : public Process
+        {
+        public:
+            CallBack(C callback) : callback_(callback)
+            {}
+
+            void Init() override
+            {
+                callback_();
+            }
+
+            void Update(double delta)
+            {
+                Stop();
+            }
+
+        private:
+            C callback_;
+        };
+
+        template<typename T>
+        class ProcessWrapper
+        {
+        public:
+            ProcessWrapper(T&& t) : t_(std::move(t))
+            {}
+            ProcessWrapper(const T& t) : t_(t)
+            {}
+
+            void Init() { t_.Init(); }
+            void Update(double delta) { t_.Update(delta); }
+            void SetNext(ProcessPtr n) { t_.SetNext(std::move(n)); }
+            const Process* GetNext() const { return t_.GetNext(); }
+            Process* GetNext() { return t_.GetNext(); }
+            ProcessPtr TakeNext() { return t_.TakeNext(); }
+            void Stop() { t_.Stop(); }
+            Process::State GetState() { return t_.GetState(); }
+        private:
+            T t_;
+        };
+
+        template<typename T>
+        class ProcessWrapper<std::unique_ptr<T>>
+        {
+        public:
+            ProcessWrapper(std::unique_ptr<T> t) : t_(std::move(t))
+            {}
+
+            void Init() { t_->Init(); }
+            void Update(double delta) { t_->Update(delta); }
+            void SetNext(ProcessPtr n) { t_->SetNext(std::move(n)); }
+            const Process* GetNext() const { return t_->GetNext(); }
+            Process* GetNext() { return t_->GetNext(); }
+            ProcessPtr TakeNext() { return t_->TakeNext(); }
+            void Stop() { t_->Stop(); }
+            Process::State GetState() { return t_->GetState(); }
+        private:
+            std::unique_ptr<T> t_;
+        };
     }
 }
